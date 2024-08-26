215aeeb6ec10c9b4311a1317441707f212c20279
Maxim_Titov <73597552+Maxim-Titov@users.noreply.github.com>
2024-08-26 12:04:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
+});
+
+$(document).ready(function(){
+    $('.mentors-slider').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
+});
+
+$(document).ready(function(){
+    $('.about-mob').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
+});
