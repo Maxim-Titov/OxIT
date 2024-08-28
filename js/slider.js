06c4cd64f2f4bf922a4b3372991101af8e5593b8
Maxim_Titov <73597552+Maxim-Titov@users.noreply.github.com>
2024-08-28 15:14:01 +0300

    Site adaptation changes

---
+++ js/slider.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false
+    });
+});
+
+$(document).ready(function(){
+    $('.mentors-slider').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false
+    });
+});
+
+$(document).ready(function(){
+    $('.about-mob').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false
+    });
+});
